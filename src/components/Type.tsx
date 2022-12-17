e848925a11d64de2f4894ac85c98662a819d0b3f
Belisy <lssoy77@gmail.com>
2022-12-18 04:26:40 +0900

    Feat: Home화면 데이터 fetch 및 데이터 가공

---
+++ src/components/Type.tsx
@@ -0,0 +1,12 @@
+export interface FetchData {
+  id: number;
+  author: string;
+  download_url: string;
+}
+
+export interface Data {
+  id: number;
+  name: string;
+  url: string;
+  thumbnail: string;
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,48 @@
-import React from "react";
+import { FetchData, Data } from "@components/Type";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
+  const [data, setData] = useState<Data[]>([]);
+  const [page, setPage] = useState(1);
+
+  const fetchData = async () => {
+    const { data } = await axios.get(`https://picsum.photos/v2/list?page=${page}`);
+    const homeItems = data.map((el: FetchData) => ({
+      id: el.id,
+      name: `Photo by ${el.author}`,
+      url: el.download_url,
+      thumbnail: el.download_url,
+    }));
+
+    console.log("data", data);
+    console.log("homeItems", homeItems);
+    setData(homeItems);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    // eslint-disable-next-line react/jsx-no-useless-fragment
-    <>
-      <div>홈 화면</div>
-    </>
+    <div>
+      <h1>홈</h1>
+      <div style={{ display: "flex", flexWrap: "wrap" }}>
+        {data &&
+          data.map((el) => (
+            <div key={el.id}>
+              <Link to={`/detail/${el.id}`}>
+                <div>
+                  <img src={el.thumbnail} alt={`이미지-${el.id}`} style={{ width: "100px" }} />
+                </div>
+              </Link>
+            </div>
+          ))}
+      </div>
+
+      <button onClick={fetchData}>Generate</button>
+    </div>
   );
 }
 
